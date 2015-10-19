aa751bbd6145cc5eaa74b9158062886c9d3ddd61
Mukul jain <jainmukul1996@gmail.com>
2015-10-19 13:50:54 +0530

    stamplay

---
+++ js/EventController.js
@@ -0,0 +1,22 @@
+app.controller('EventController',['$scope', function($scope){
+
+	$scope.polys = [
+	{
+		name : 'competition',
+	},
+	{
+		name : 'MUN',
+	},
+	{
+		name : 'Workshops',
+	},
+	{
+		name : 'Talks',
+	},
+	{
+		name : 'Exhibations',
+	}
+
+	];
+
+}]);
